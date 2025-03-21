4d5af3c0c8a5e3222760c5d9583632b163c84dde
Mauricio Stamatti <mauriciostamatti@gmail.com>
2025-03-21 15:01:17 -0300

    Ejercicio 3 terminado

--- firmware/projects/guia1_ej3/main/guia1_ej3.c
+++ firmware/projects/guia1_ej3/main/guia1_ej3.c
@@ -17,17 +17,21 @@
  #include <stdio.h>
  #include <stdint.h>
  #include <stdbool.h>
+ #include <string.h>
  #include "freertos/FreeRTOS.h"
  #include "freertos/task.h"
  #include "led.h"
  #include "switch.h"
  
+ 
  #define CONFIG_BLINK_PERIOD 1000
- #define TOGGLE 2  // Definir TOGGLE si no existe
+ #define ON 0
+ #define OFF 1
+ #define TOGGLE 2
  
  struct leds
  {
-	 uint8_t mode;       
+	uint8_t mode;
 	 uint8_t n_led;        
 	 uint8_t n_ciclos;   
 	 uint16_t periodo;    
@@ -35,67 +39,48 @@
  
  void Funcion_prueba(struct leds *led)
  {
-	 if (led->mode == 1)  // Encender LED
-	 {
-		switch (led->n_led)
+	switch (led->mode)
+	{
+	case ON: // Encender LED
+	switch (led->n_led)
+	{
+	case 1:
+		LedOn(LED_1);
+		break;
+	case 2:
+		LedOn(LED_2);
+		break;
+	
+	default:
+		LedOn(LED_3);
+		break;
+	}
+		break;
+
+	
+	case OFF: // Apagar LED
+	switch (led->n_led)
+	{
+	case 1:
+		LedOff(LED_1);
+		break;
+	case 2:
+		LedOff(LED_2);
+		break;
+	
+	default:
+		LedOff(LED_3);
+		break;
+	}
+		break;
+
+
+	case TOGGLE: // Parpadear LED
+	int i=0;	
+		while ( i < led->n_ciclos*2)
 		{
-		case 1:
-			LedOn(LED_1);
-			break;
-		case 2:
-			LedOn(LED_2);
-			break;
 		
-		default:
-			LedOn(LED_3);
-			break;
-		}
-		 /*if (led->n_led == 1)
-		 {
-			 LedOn(LED_1);
-		 }
-		 else if (led->n_led == 2)
-		 {
-			 LedOn(LED_2);
-		 }
-		 else
-		 {
-			 LedOn(LED_3);
-		 }*/
-	 }
-	 else if (led->mode == 0)  // Apagar LED
-	 { 
 		switch (led->n_led)
-		{
-		case 1:
-			LedOff(LED_1);
-			break;
-		case 2:
-			LedOff(LED_2);
-			break;
-		
-		default:
-			LedOff(LED_3);
-			break;
-		}
-		 /*if (led->n_led == 1)
-		 {
-			 LedOff(LED_1);
-		 }
-		 else if (led->n_led == 2)
-		 {
-			 LedOff(LED_2);
-		 }
-		 else
-		 {
-			 LedOff(LED_3);
-		 }*/
-	 }
-	 else if (led->mode == TOGGLE)  // Parpadear LED
-	 {
-		 for (int i = 0; i < led->n_ciclos; i++)
-		 {
-			switch (led->n_led)
 		{
 		case 1:
 		LedToggle(LED_1);
@@ -108,33 +93,33 @@
 		LedToggle(LED_3);
 			break;
 		}
-			/* if (led->n_led == 1)
-			 {
-				 LedToggle(LED_1);
-			 }
-			 else if (led->n_led == 2)
-			 {
-				 LedToggle(LED_2);
-			 }
-			 else
-			 {
-				 LedToggle(LED_3);
-			 }*/
-			 vTaskDelay(led->periodo / portTICK_PERIOD_MS);
-		 }
-	 }
+			
+		for (size_t j = 0; j < 10; j++)
+		{
+			vTaskDelay(led->periodo / portTICK_PERIOD_MS);
+		}	
+				i++;
+			}
+				break;
+					
+			default:
+				break;
+			
+}
+	 
  }
+
+
  
  void app_main(void){
-	 uint8_t teclas;
 	 LedsInit();
 	 SwitchesInit();
  
 	 struct leds my_leds;
-	 my_leds.mode = 2;
-	 my_leds.n_led = 1;
-	 my_leds.n_ciclos = 10;
-	 my_leds.periodo = 100;
+	 my_leds.mode = TOGGLE;
+	 my_leds.n_led = 3;
+	 my_leds.n_ciclos = 15;
+	 my_leds.periodo = 500;
  
 	 Funcion_prueba(&my_leds);
  }
