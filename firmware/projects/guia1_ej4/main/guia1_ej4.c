6c20a56e280e0a37a47ddf21cf5e502f7866eeff
Mauricio Stamatti <mauriciostamatti@gmail.com>
2025-03-21 16:08:31 -0300

    Creacion carpeta ejercicio 4

---
+++ firmware/projects/guia1_ej4/main/guia1_ej4.c
@@ -0,0 +1,43 @@
+/*! @mainpage Template
+ *
+ * @section genDesc General Description
+ *
+ * This section describes how the program works.
+ *
+ * <a href="https://drive.google.com/...">Operation Example</a>
+ *
+ * @section hardConn Hardware Connection
+ *
+ * |    Peripheral  |   ESP32   	|
+ * |:--------------:|:--------------|
+ * | 	PIN_X	 	| 	GPIO_X		|
+ *
+ *
+ * @section changelog Changelog
+ *
+ * |   Date	    | Description                                    |
+ * |:----------:|:-----------------------------------------------|
+ * | 21/03/2025 | Document creation		                         |
+ *
+ * @author Mauricio Stamatti
+ *
+ */
+
+/*==================[inclusions]=============================================*/
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <array>
+#include <cstdint>
+using namespace std;
+
+array <uint8_t, 31> bcd_number;
+int8_t  convertToBcdArray(uint32_t data, uint8_t digits, uint8_t * bcd_number)
+{
+	
+}
+
+
+void app_main(void){
+	
+}
