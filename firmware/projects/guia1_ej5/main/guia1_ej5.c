72322c63276e22a4764b495aa45b593625ef6f68
Mauricio Stamatti <mauriciostamatti@gmail.com>
2025-03-28 16:46:04 -0300

    Super codigo bcd

--- firmware/projects/guia1_ej5/main/guia1_ej5.c
+++ firmware/projects/guia1_ej5/main/guia1_ej5.c
@@ -59,6 +59,19 @@ void config_gpio(gpioConf_t *gpio, uint8_t bcd_number)
 	}
 }
 
+void Grafica_bcd(uint32_t datos, uint8_t digitos_salida, gpioConf_t *gpio_1, gpioConf_t *gpio_2)
+{
+	uint8_t numero_por_partes[3] = {0};
+	convertToBcdArray(datos, digitos_salida, &numero_por_partes[0]);
+
+	for (int i = 0; i < 3; i++)
+	{
+		config_gpio(gpio_2, numero_por_partes[i]);
+		GPIOOn(gpio_1[i].pin);
+		GPIOOff(gpio_1[i].pin);
+	}
+}
+
 void app_main(void)
 {
 
@@ -74,8 +87,20 @@ void app_main(void)
 	{
 		GPIOInit(gpio[i].pin, gpio[i].dir);
 	}
-	config_gpio(gpio, 8);
-	convertToBcdArray(123, 3, &bcd_numer[0]);
+
+	gpioConf_t gpio_pantalla[3] =
+		{
+			{GPIO_19, GPIO_OUTPUT},
+			{GPIO_18, GPIO_OUTPUT},
+			{GPIO_9, GPIO_OUTPUT}};
+
+	for (int i = 0; i < 3; i++)
+	{
+		GPIOInit(gpio_pantalla[i].pin, gpio_pantalla[i].dir);
+	}
+
+	// convertToBcdArray(123, 3, &bcd_numer[0]);
+	Grafica_bcd(69, 3, &gpio_pantalla[0], &gpio[0]);
 
 	printf("BCD: %d %d %d\n", bcd_numer[0], bcd_numer[1], bcd_numer[2]);
 }
