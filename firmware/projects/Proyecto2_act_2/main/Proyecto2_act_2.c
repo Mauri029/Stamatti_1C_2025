50d546d3bddc985db9e62ee1ea97ab5de09a38cb
Mauricio Stamatti <mauriciostamatti@gmail.com>
2025-04-25 16:34:14 -0300

    Actividad dos sin terminar, proyecto 2

---
+++ firmware/projects/Proyecto2_act_2/main/Proyecto2_act_2.c
@@ -0,0 +1,147 @@
+/*! @mainpage Actividad 2 del proyecto 2
+ *
+ * @section genDesc General Description
+ *
+ *El programa mide la distancia , prende los led segun la distancia que mida, muestra la distancia en una
+ * pantalla lcd y prende o apaga la medicion y mantiene el valor en pantalla con interrupciones
+ *
+ *
+ * @section hardConn Hardware Connection
+ * \brief Driver for using the 3 digits numeric display in ESP-EDU.
+ *
+ * |   Display      |   EDU-CIAA	|
+ * |:--------------:|:-------------:|
+ * | 	Vcc 	    |	5V      	|
+ * | 	BCD1		| 	GPIO_20		|
+ * | 	BCD2	 	| 	GPIO_21		|
+ * | 	BCD3	 	| 	GPIO_22		|
+ * | 	BCD4	 	| 	GPIO_23		|
+ * | 	SEL1	 	| 	GPIO_19		|
+ * | 	SEL2	 	| 	GPIO_18		|
+ * | 	SEL3	 	| 	GPIO_9		|
+ * | 	Gnd 	    | 	GND     	|
+ *
+ * \brief Driver for reading distance with HC-SR04 module.
+ * |   HC_SR04      |   EDU-CIAA	|
+ * |:--------------:|:-------------:|
+ * | 	Vcc 	    |	5V      	|
+ * | 	Echo		| 	GPIO_3		|
+ * | 	Trig	 	| 	GPIO_2		|
+ * | 	Gnd 	    | 	GND     	|
+ *
+ *
+ *
+ *
+ * @section changelog Changelog
+ *
+ * |   Date	    | Description                                    |
+ * |:----------:|:-----------------------------------------------|
+ * | 12/09/2023 | Document creation		                         |
+ *
+ * @author Mauricio Stamatti
+ *
+ */
+
+/*==================[inclusions]=============================================*/
+#include <stdio.h>
+#include <stdint.h>
+#include "lcditse0803.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "led.h"
+#include "hc_sr04.h"
+#include "lcditse0803.h"
+#include "switch.h"
+#include "timer_mcu.h"
+
+/*==================[macros and definitions]=================================*/
+bool on_off_pantalla = true;
+bool hold = false;
+uint16_t distancia;
+uint8_t teclas;
+
+/*==================[internal data definition]===============================*/
+TaskHandle_t Medir_task_handle = NULL;
+/*==================[internal functions declaration]=========================*/
+static void Medir_task()
+{
+	while (true)
+	{
+		//ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+		if (on_off_pantalla)
+		{
+			distancia = HcSr04ReadDistanceInCentimeters();
+
+			if (distancia <= 10)
+			{
+				LedOff(LED_1);
+				LedOff(LED_2);
+				LedOff(LED_3);
+			}
+			else if (distancia > 10 && distancia <= 20)
+			{
+				LedOn(LED_1);
+				LedOff(LED_2);
+				LedOff(LED_3);
+			}
+			else if (distancia > 20 && distancia <= 30)
+			{
+				LedOn(LED_1);
+				LedOn(LED_2);
+				LedOff(LED_3);
+			}
+			else
+			{
+				LedOn(LED_1);
+				LedOn(LED_2);
+				LedOn(LED_3);
+			}
+			if (!hold)
+			{
+				LcdItsE0803Write(distancia);
+			}
+		}
+		else
+		{
+			LcdItsE0803Off();
+			LedsOffAll();
+		}
+	}
+}
+
+void On_off_pantalla()
+{
+	on_off_pantalla = !on_off_pantalla;
+}
+
+void Hold()
+{
+	hold = !hold;
+}
+
+void FuncTimerA(void *param)
+{
+	vTaskNotifyGiveFromISR(Medir_task_handle, pdFALSE);
+}
+/*==================[external functions definition]==========================*/
+void app_main(void)
+{
+	HcSr04Init(GPIO_3, GPIO_2);
+	LcdItsE0803Init();
+	LedsInit();
+	SwitchesInit();
+
+	timer_config_t timer_led_1 = {
+		.timer = TIMER_A,
+		.period = 1000000,
+		.func_p = FuncTimerA,
+		.param_p = NULL};
+
+	TimerInit(&timer_led_1);
+
+	SwitchActivInt(SWITCH_1, On_off_pantalla, NULL);
+	SwitchActivInt(SWITCH_2, Hold, NULL);
+	xTaskCreate(&Medir_task, "Medir", 2048, NULL, 1, NULL);
+	TimerStart(timer_led_1.timer);
+}
+/*==================[end of file]============================================*/
