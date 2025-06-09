5054cc0f86fb87be2339119aa13b14186a56b26b
Mauricio Stamatti <mauriciostamatti@gmail.com>
2025-06-09 17:49:05 -0300

    realizada la funcion de de lectuara analogica y las acciones segun la radiacion, tambien se inicializo el puerto uart

--- firmware/projects/Examen_final_programable/main/Examen_final_programable.c
+++ firmware/projects/Examen_final_programable/main/Examen_final_programable.c
@@ -39,53 +39,91 @@
 #include "dht11.h"
 /*==================[macros and definitions]=================================*/
 #define CONFIG_BLINK_PERIOD_sensor_presion_temperatura 1000
+#define CONFIG_BLINK_PERIOD_sensor_radiacion 5000
+
+bool estado_sensores_y_notificacion = true; // Variable para controlar el estado de los sensores y la notificación
+float humedad = 0, temperatura = 0;
+uint16_t radicacion_leida=0;
+
 /*==================[internal data definition]===============================*/
-TaskHandle_t medir_presion_y_temeratura_task_handle = NULL;
+TaskHandle_t medir_presion_y_temperatura_task_handle = NULL;
+TaskHandle_t medir_radiacion_task_handle = NULL;
+
 /*==================[internal functions declaration]=========================*/
-static void medir_presion_y_temeratura_task()
+static void medir_presion_y_temperatura_task()
 {
 	while (true)
 	{
-
-		float humedad = 0, temperatura = 0;
-		dht11Read(&humedad, &temperatura);
-		if (humedad > 85 && (temperatura > 0 && temperatura < 2))
+		if (estado_sensores_y_notificacion)
 		{
-			LedOn(LED_3); // led rojo
-			AnalogInputReadSingle(CH1, &temperatura);
-			UartSendString(UART_PC, ">Temperatura: ");
-			UartSendString(UART_PC, (char *)UartItoa(temperatura, 10));
-			UartSendString(UART_PC, "°C- ");
-			AnalogInputReadSingle(CH2, &humedad);
-			UartSendString(UART_PC, ">Humedad: ");
-			UartSendString(UART_PC, (char *)UartItoa(humedad, 10));
-			UartSendString(UART_PC, "%");
-			UartSendString(UART_PC, " - RIESGO DE NEVADA");
-			UartSendString(UART_PC, "\r\n");
+			
+			dht11Read(&humedad, &temperatura);
+			if (humedad > 85 && (temperatura > 0 && temperatura < 2))
+			{
+				LedOn(LED_3);  // led rojo
+				LedOff(LED_1); // led verde
+				UartSendString(UART_PC, ">Temperatura: ");
+				UartSendString(UART_PC, (char *)UartItoa(temperatura, 10));
+				UartSendString(UART_PC, "°C- ");
+				UartSendString(UART_PC, ">Humedad: ");
+				UartSendString(UART_PC, (char *)UartItoa(humedad, 10));
+				UartSendString(UART_PC, "%");
+				UartSendString(UART_PC, " - RIESGO DE NEVADA");
+				UartSendString(UART_PC, "\r\n");
+			}
+			else
+			{
+				LedOn(LED_1);  // led verde
+				LedOff(LED_3); // led rojo
+				UartSendString(UART_PC, ">Temperatura: ");
+				UartSendString(UART_PC, (char *)UartItoa(temperatura, 10));
+				UartSendString(UART_PC, "°C- ");
+				UartSendString(UART_PC, ">Humedad: ");
+				UartSendString(UART_PC, (char *)UartItoa(humedad, 10));
+				UartSendString(UART_PC, "%");
+				UartSendString(UART_PC, "\r\n");
+			}
 		}
-		else
-		{
-			LedOn(LED_1); // led verde
-			AnalogInputReadSingle(CH1, &temperatura);
-			UartSendString(UART_PC, ">Temperatura: ");
-			UartSendString(UART_PC, (char *)UartItoa(temperatura, 10));
-			UartSendString(UART_PC, "°C- ");
-			AnalogInputReadSingle(CH2, &humedad);
-			UartSendString(UART_PC, ">Humedad: ");
-			UartSendString(UART_PC, (char *)UartItoa(humedad, 10));
-			UartSendString(UART_PC, "%");
-			UartSendString(UART_PC, "\r\n");
-		}
-			vTaskDelay(CONFIG_BLINK_PERIOD_sensor_presion_temperatura / portTICK_PERIOD_MS);
-
-		
-
-
+		vTaskDelay(CONFIG_BLINK_PERIOD_sensor_presion_temperatura / portTICK_PERIOD_MS);
 	}
 }
-
-void medir_radicacion();
+uint16_t leer_entrada_analogica()
 {
+	AnalogInputReadSingle(CH1, &radicacion_leida);
+	radicacion_leida = (radicacion_leida /4095)*100; // lo convierte a mR/h, siendo 100 el maximo
+	return radicacion_leida;
+
+}
+
+static void medir_radicacion_task()
+{
+	while (true)
+	{
+		if (estado_sensores_y_notificacion)
+		{
+			if(leer_entrada_analogica()>40)
+			{
+				LedOn(LED_2);  // led amarillo
+				LedOff(LED_1); // led verde
+				UartSendString(UART_PC, ">Radiacion: ");
+				UartSendString(UART_PC, (char *)UartItoa(radicacion_leida, 10));
+				UartSendString(UART_PC, " mR/h ");
+				UartSendString(UART_PC, " - RADIACION ELEVADA");
+				UartSendString(UART_PC, "\r\n");
+			} 
+			else 
+			{
+				LedOff(LED_2); // led amarillo
+				LedOn(LED_1);  // led verde
+				UartSendString(UART_PC, ">Radiacion: ");
+				UartSendString(UART_PC, (char *)UartItoa(radicacion_leida, 10));
+				UartSendString(UART_PC, " mR/h ");
+				UartSendString(UART_PC, "\r\n");
+			}
+
+		}
+		vTaskDelay(CONFIG_BLINK_PERIOD_sensor_presion_temperatura / portTICK_PERIOD_MS);
+	}
 }
 
 void radiacion_elevada()
@@ -93,21 +131,43 @@ void radiacion_elevada()
 	LedOn(LED_2); // led amarillo
 }
 
-void riesgo_nevada()
+void apagar_sensores_y_notificacion()
 {
-	LedOn(LED_3); // led rojo
+	estado_sensores_y_notificacion = false;
 }
+
+void prender_sensores_y_notificacion()
+{
+	estado_sensores_y_notificacion = true;
+}
+
 /*==================[external functions definition]==========================*/
 void app_main(void)
 {
+	analog_input_config_t adc_config = {
+		CH1,		/*!< Inputs: CH0, CH1, CH2, CH3 */
+		ADC_SINGLE, /*!< Mode: single read or continuous read */
+		NULL,		/*!< Pointer to callback function for convertion end (only for continuous mode) */
+		NULL,
+		0, /*!< Pointer to callback function parameters (only for continuous mode) */
+	};
+
+	AnalogInputInit(&adc_config);
+
+	serial_config_t my_uart = {
+		.baud_rate = 9600,
+		.port = UART_PC,
+		.func_p = NULL,
+		.param_p = NULL,
+	};
+	UartInit(&my_uart);
 
 	dht11Init(GPIO_6);
 	LedsInit();
 	SwitchesInit();
 
-
-	SwitchActivInt(SWITCH_1, On_off_pantalla, NULL);
-	SwitchActivInt(SWITCH_2, Hold, NULL);
-	xTaskCreate(&medir_presion_y_temeratura, "Medir", 2048, NULL, 5, &medir_presion_y_temeratura_task_handle);
+	SwitchActivInt(SWITCH_1, prender_sensores_y_notificacion, NULL);
+	SwitchActivInt(SWITCH_2, apagar_sensores_y_notificacion, NULL);
+	xTaskCreate(&medir_presion_y_temperatura_task, "Medir", 2048, NULL, 5, &medir_presion_y_temperatura_task_handle);
 }
 /*==================[end of file]============================================*/
